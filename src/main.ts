7e09dec0c4a9db773b7111375f0ec0657519cd1a
paretk7 <paretk7@gmail.com>
2020-11-25 10:24:15 +0700

    feat: add guard

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,7 @@ async function bootstrap() {
   .setDescription('Coffee application')
   .setVersion('1.0')
   .build();
-
+  app.useGlobalGuards(new ApiKeyGuard())
   const document = SwaggerModule.createDocument(app, options);
 
   SwaggerModule.setup('api', app, document);
